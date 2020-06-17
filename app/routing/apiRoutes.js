265cc599420858637be58d2cbb79c5a7ade5c1fe
kallistahalim <kallistahalim@gmail.com>
2020-06-16 17:02:15 -0700

    hey they link together

---
+++ app/routing/apiRoutes.js
@@ -0,0 +1,19 @@
+var path = require("path");
+
+function apiRoutes(app) {
+    app.get("api/friends", function(req, res) {
+        return res.json(friends);
+    });
+
+    app.post("api/friends", function(req, res) {
+        var buddy = req.body;
+
+        buddy.routeName = buddy.name.replace(/\s+/g, "").toLowerCase();
+
+        friends.push(buddy);
+
+        res.json(buddy);
+    });
+};
+
+module.exports = apiRoutes;
